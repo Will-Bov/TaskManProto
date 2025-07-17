aedad42103656414b0df808f9fa0435dddbe1c4e
[dyad] <git@dyad.sh>
2025-07-17 20:34:55 +1000

    [dyad] Added missing Label import to TaskManager - wrote 1 file(s)

--- src/components/task-manager/TaskManager.tsx
+++ src/components/task-manager/TaskManager.tsx
@@ -6,6 +6,7 @@ import { TaskForm } from "./TaskForm";
 import { TaskItem } from "./TaskItem";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Card } from "@/components/ui/card";
 
 export function TaskManager() {
